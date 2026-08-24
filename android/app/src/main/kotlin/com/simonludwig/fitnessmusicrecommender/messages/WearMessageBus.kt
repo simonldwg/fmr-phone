dc0f2cba077c6e6459fc6125ba36db36d81f9967
simon <info@simonludwig.com>
2026-08-24 21:55:32 +0200

    add message buffer

--- android/app/src/main/kotlin/com/simonludwig/fitnessmusicrecommender/messages/WearMessageBus.kt
+++ android/app/src/main/kotlin/com/simonludwig/fitnessmusicrecommender/messages/WearMessageBus.kt
@@ -1,11 +1,12 @@
 package com.simonludwig.fitnessmusicrecommender.messages
 
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
 object WearMessageBus {
-    private val _messages = MutableSharedFlow<Map<String, Any>>()
+    private val _messages = MutableSharedFlow<Map<String, Any>>(replay = 0, extraBufferCapacity = 4, onBufferOverflow = BufferOverflow.DROP_OLDEST)
     val messages: SharedFlow<Map<String, Any>> = _messages.asSharedFlow()
 
     suspend fun emit(map: Map<String, Any>) = _messages.emit(map)
